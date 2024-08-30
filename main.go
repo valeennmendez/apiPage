1d54b593f55ad87cd84a8bf0e9fa8f0b6aa5631f
valeennmendez <112477437+valeennmendez@users.noreply.github.com>
2024-08-30 05:49:27 -0300

    Update main.go

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://127.0.0.1:3000"},
+		AllowOrigins:     []string{"https://apipage-production-f781.up.railway.app/"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
