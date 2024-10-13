8a9990c108cb26c0c9612d15951296c1bb948add
valeennmendez <112477437+valeennmendez@users.noreply.github.com>
2024-10-13 19:55:56 -0300

    Update dbConnection.go

--- connection/dbConnection.go
+++ connection/dbConnection.go
@@ -3,18 +3,19 @@ package connection
 import (
 	"fmt"
 
-	"gorm.io/driver/mysql"
+	//"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
-//const DSN = "root:eTmzFGVqnyRPratogumTycVamwXDKfgh@tcp(junction.proxy.rlwy.net:36199)/railway?charset=utf8mb4&parseTime=True&loc=Local"
-const DSN = "root:fckuftiuyWOiPpsvcGlBIgTSSlnvgSXB@tcp(junction.proxy.rlwy.net:15131)/railway?charset=utf8mb4&parseTime=True&loc=Local"
+// const DSN = "root:@tcp(127.0.0.1:3306)/Acneclinic?charset=utf8mb4&parseTime=True&loc=UTC"
+const DSN = "postgresql://postgres.vfbshxvqidlrawostjvn:Repeatrave10_@aws-0-us-west-1.pooler.supabase.com:6543/postgres"
 
 func ConnectionDB() {
 	var err error
-	DB, err = gorm.Open(mysql.Open(DSN), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(DSN), &gorm.Config{})
 
 	if err != nil {
 		panic(err)
@@ -22,4 +23,5 @@ func ConnectionDB() {
 
 	fmt.Println("Base de Datos corriendo...")
 
-} 
+}
+ 
